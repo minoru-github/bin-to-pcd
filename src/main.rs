27ba1229d5882d361663f6866cf7bf509a929f0c
minoru-github <kurami0621@yahoo.co.jp>
2022-07-07 23:18:52 +0900

    feat: binaryデータを読み込み[x, y, z, i]に変換

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,55 @@
-use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::{env, mem};
 
 fn main() {
     //let args: Vec<String> = env::args().collect();
     //let filename = &args[1];
     let filename = "./src/data/0000000000.bin";
-
-    println!("file name : {}", filename);
-
-    let mut f = File::open(filename).expect("file not found");
     let mut buffer = Vec::new();
+    read_binary_file(filename, &mut buffer);
+
+    let mut pcd_lines = Vec::<Vec<f32>>::new();
+    to_pcd(&buffer, &mut pcd_lines);
+}
+
+fn read_binary_file(filename: &str, mut buffer: &mut Vec<u8>) {
+    let mut f = File::open(filename).expect("file not found");
     f.read_to_end(&mut buffer).expect("read error");
-    unsafe {
-        let ptr = buffer.as_ptr();
-        let arr = std::slice::from_raw_parts(ptr, 4);
-        let data = arr as *const [u8] as *const f32;
-        println!("{}", *data);
+}
+
+fn to_pcd(input_buffer: &Vec<u8>, output_pcd_lines: &mut Vec<Vec<f32>>) {
+    const PCD_ELEMS: usize = 4; // [x, y, z, i]
+
+    let bytes_of_f32 = mem::size_of::<f32>();
+    let bytes_per_line = bytes_of_f32 * PCD_ELEMS;
+    let lines = input_buffer.len() / bytes_per_line;
+
+    let ptr = input_buffer.as_ptr();
+    let mut ptr_offset = 0;
+    for _ in 0..lines {
+        // xyzi
+        let mut pcd = Vec::new();
+        for _ in 0..PCD_ELEMS {
+            unsafe {
+                let array_u8 = std::slice::from_raw_parts(ptr.add(ptr_offset), bytes_of_f32);
+                let data = (array_u8 as *const [u8]) as *const f32;
+                pcd.push(*data);
+                ptr_offset += bytes_of_f32;
+            }
+        }
+        output_pcd_lines.push(pcd);
     }
-    let buf : [u8;4] = [0x83,0xC0,0xB5,0x41];
-    unsafe {
-        let p = &buf as *const [u8] as *const f32;
-        println!("{}", *p);
+}
+
+#[allow(dead_code)]
+fn debug_pcd(pcd_lines: &Vec<Vec<f32>>) {
+    let mut cnt = 0;
+    for pcd in pcd_lines {
+        println!("{:?}", pcd);
+        cnt += 1;
+        if cnt >= 10 {
+            break;
+        }
     }
 }
