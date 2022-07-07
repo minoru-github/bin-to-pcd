215b2a562e011b5c4f9e5495f16c366aa4b84344
minoru-github <kurami0621@yahoo.co.jp>
2022-07-07 19:22:07 +0900

    feat: 4byteのu8をf32に変換

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    let buf : [u8;4] = [0x83,0xC0,0xB5,0x41];
+    unsafe {
+        let p = &buf as *const [u8] as *const f32;
+        println!("{}", *p);
+    }
 }
